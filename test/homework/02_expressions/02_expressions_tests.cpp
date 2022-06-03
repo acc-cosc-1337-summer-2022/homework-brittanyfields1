2a762bfccddc8d1a436d0bcd6741b6f95375baaa
brittanyfields1 <brittany.fields2@g.austincc.edu>
2022-06-03 02:24:37 +0000

    assignment 1

--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "hwexpressions.h"
+#include "mult-assign.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -12,3 +13,6 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE ("Test function multiply numbers")
+	REQUIRE (multiply_numbers(10)== 50)
+	REQUIRE (multiply_numbers(2)== 10)
