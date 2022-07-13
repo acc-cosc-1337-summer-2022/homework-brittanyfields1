c8ebafce1712ae2ed2316686eb54add253ed9d3e
brittanyfields1 <brittany.fields2@g.austincc.edu>
2022-07-13 22:43:56 +0000

    assignment7

--- src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_4.cpp
@@ -13,9 +13,21 @@ Win by column if and return true if (each column index)
 else
 false
 */
-
-
-
+tictactoe4::check_column_win()
+{ 
+    if((pegs[0] == "X" && pegs[4] == "X" && pegs[8] == "X" && pegs[12] == "X") || 
+    (pegs[1] == "X" && pegs[5] == "X" && pegs[9] == "X" && pegs[13] == "X") ||
+    (pegs[2] == "X" && pegs[6] == "X" && pegs[10] == "X" && pegs[14] == "X") ||
+    (pegs[3] == "X" && pegs[7] == "X" && pegs[11] == "X" && pegs[15] == "X") ||
+    (pegs[0] == "O" && pegs[4] == "O" && pegs[8] == "O" && pegs[12] == "O") || 
+    (pegs[1] == "O" && pegs[5] == "O" && pegs[9] == "O" && pegs[13] == "O") ||
+    (pegs[2] == "O" && pegs[6] == "O" && pegs[10] == "O" && pegs[14] == "O") ||
+    (pegs[3] == "O" && pegs[7] == "O" && pegs[11] == "O" && pegs[15] == "O"))
+       {
+           return true;
+       }
+    return false;
+}
 
 /*
 class function check_row_win
@@ -25,7 +37,21 @@ Win by row if
 8, 9, 10, 11 are equal 
 12,13,14, 15 are equal
 */
-
+tictactoe4::check_row_win()
+{ 
+    if((pegs[0] == "X" && pegs[1] == "X" && pegs[2] == "X" && pegs[3] == "X") || 
+    (pegs[4] == "X" && pegs[5] == "X" && pegs[6] == "X" && pegs[7] == "X") ||
+    (pegs[8] == "X" && pegs[9] == "X" && pegs[10] == "X" && pegs[11] == "X") ||
+    (pegs[12] == "X" && pegs[13] == "X" && pegs[14] == "X" && pegs[15] == "X") ||
+    (pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O" && pegs[3] == "O") || 
+    (pegs[4] == "O" && pegs[5] == "O" && pegs[6] == "O" && pegs[7] == "O") ||
+    (pegs[8] == "O" && pegs[9] == "O" && pegs[10] == "O" && pegs[11] == "O") ||
+    (pegs[12] == "O" && pegs[13] == "O" && pegs[14] == "O" && pegs[15] == "O"))
+       {
+           return true;
+       }
+    return false;
+}
 
 
 /*
@@ -37,3 +63,16 @@ Win diagonally
 12,13,14, 15
 
 */
+
+tictactoe4 :: check_diagonal_win()
+{ 
+    if((pegs[3] == "X" && pegs[6] == "X" && pegs[9] == "X" && pegs[12] == "X") || 
+    (pegs[0] == "X" && pegs[5] == "X" && pegs[10] == "X" && pegs[15] == "X") ||
+    (pegs[3] == "O" && pegs[6] == "O" && pegs[9] == "O" && pegs[12] == "O") ||
+    (pegs[0] == "X" && pegs[5] == "X" && pegs[10] == "X" && pegs[15] == "X"))
+    
+       {
+           return true;
+       }
+    return false;
+}
