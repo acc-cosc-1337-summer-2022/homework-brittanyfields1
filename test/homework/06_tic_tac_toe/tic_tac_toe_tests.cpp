e974b17be8fa39e9a5663ad834e48f46f3cf2864
brittanyfields1 <106637818+brittanyfields1@users.noreply.github.com>
2022-07-16 20:54:35 -0500

    homework7 test cases

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -14,26 +14,26 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE ("Test if the board is full")
 {
 	unique_ptr<TicTacToe> game = make_unique<tictactoe3>();
-	game.mark_board(1);
+	game->mark_board(1);
 	REQUIRE(game.game_over() == false);
-	game.mark_board(2);
+	game->mark_board(2);
 	REQUIRE(game.game_over()== false);
-	game.mark_board(3);
+	game->mark_board(3);
 	REQUIRE(game.game_over()== false);
-	game.mark_board(4);
+	game->mark_board(4);
 	REQUIRE(game.game_over()== false);
-	game.mark_board(5);
+	game->mark_board(5);
 	REQUIRE(game.game_over() == false);
-	game.mark_board(7);
+	game->mark_board(7);
 	REQUIRE(game.game_over()== false);
-	game.mark_board(6);
+	game->mark_board(6);
 	REQUIRE(game.game_over()== false);
-	game.mark_board(9);
+	game->mark_board(9);
 	REQUIRE(game.game_over()== false);
-	game.mark_board(8);
+	game->mark_board(8);
 
-	REQUIRE(game.game_over()== true);
-	REQUIRE(game.get_winner() == "C");
+	REQUIRE(game->game_over()== true);
+	REQUIRE(game->get_winner() == "C");
 }
 
 TEST_CASE ("Test first player set to X")
